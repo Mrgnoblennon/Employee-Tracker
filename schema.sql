47a6e4b49e1cb3af069a9c23f21ad7e293804027
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-06-13 14:11:27 +0800

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,33 @@
+--drops the employee_tracker if it exists currently --
+DROP DATABASE IF EXISTS employee_tracker;
+--creates the employee_tracker database --
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+--create the department table
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+--create the role table
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+--create the employee table
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
